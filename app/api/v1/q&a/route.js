27e82ebd3c9c5c0113fd4c416c825e3384916aef
khashayar yaghma <khashayaryaghma@gmail.com>
2024-10-29 19:08:03 +0330

    add api route

--- app/api/v1/q&a/route.js
+++ app/api/v1/q&a/route.js
@@ -1,10 +1,38 @@
-
-
 export const data = [
   {
-    id:"",
+    id: "1",
+    title: "JS",
+    description: "what is js?",
+    answers: [
+      { ansText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam libero cupiditate nobis voluptas commodi ex porro corrupti officia." },
+    ],
+  },
+  {
+    id: "2",
+    title: "HTML",
+    description: "What is HTML?",
+    answers: [
+      { ansText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam libero cupiditate nobis voluptas commodi ex porro corrupti officia." },
+    ],
+  },
+  {
+    id: "3",
     title: "",
-    description: "",
-    answer: "",
+    description: "What is CSS?",
+    answers: [
+      { ansText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam libero cupiditate nobis voluptas commodi ex porro corrupti officia." },
+    ],
+  },
+  {
+    id: "4",
+    title: "React",
+    description: "what is React?",
+    answers: [
+      { ansText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam libero cupiditate nobis voluptas commodi ex porro corrupti officia." },
+    ],
   },
 ];
+
+export async function GET() {
+  return Response.json({ data });
+}
