3941c060fe88417a4b8c6e849b265810f4505edf
khashayar yaghma <khashayaryaghma@gmail.com>
2024-10-29 17:58:32 +0330

    create header

--- utils/Theme.jsx
+++ utils/Theme.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { createTheme, CssBaseline, responsiveFontSizes, ThemeProvider as ThemeProviderMui } from "@mui/material";
 import { Poppins } from "next/font/google";
 
@@ -9,6 +9,14 @@ const font = Poppins({
 });
 function ThemeProvider({ children }) {
   let theme = createTheme({
+    palette: {
+      primary: {
+        main: "#4A90E2",
+      },
+      secondary: {
+        main: "#7ED321",
+      },
+    },
     typography: {
       fontFamily: font.style.fontFamily,
     },
