570faa6382363cbf19c4fbc091a82705e33e1d7b
khashayar yaghma <khashayaryaghma@gmail.com>
2024-10-30 17:16:58 +0330

    add Hero

---
+++ components/Hero.jsx
@@ -0,0 +1,36 @@
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Link from "next/link";
+import Image from "next/image";
+
+const styles = {
+  root: {
+    padding: { xs: "7rem 0", md: "10rem 0" },
+  },
+};
+
+export const Hero = () => {
+  return (
+    <Container component="section">
+      <Stack gap="4rem" direction="row" justifyContent="space-between" alignItems="center" sx={styles.root}>
+        <Box component="article" sx={{ maxWidth: "40rem" }}>
+          <Typography gutterBottom variant="h6" letterSpacing={"5px"}>
+            Find your Answers
+          </Typography>
+          <Typography component="h1" variant="h3" gutterBottom fontWeight="bold">
+            Questions and Answers
+          </Typography>
+          <Typography gutterBottom marginBottom="3rem" variant="subtitle1">
+            Looking for answers? You've come to the right place! Our community is here to help with reliable, insightful answers to all your questions. Whether
+            you're here to learn, share your expertise, or just browse, we’re excited to have you.
+          </Typography>
+          <Link href={"/questions"} target="_blank">
+            <Button variant="contained">Go to Questions</Button>
+          </Link>
+        </Box>
+        <Box sx={{ display: { xs: "none", md: "block" } }}>
+          <Image src={"/qa.jpg"} width={400} height={270} alt="qa" />
+        </Box>
+      </Stack>
+    </Container>
+  );
+};
