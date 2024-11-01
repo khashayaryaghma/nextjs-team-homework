f8280da0af714fa95b2e35163db2690893d517c0
khashayar yaghma <khashayaryaghma@gmail.com>
2024-11-01 15:10:55 +0330

    write post

--- utils/dataServices.js
+++ utils/dataServices.js
@@ -1,8 +1,12 @@
-"use server";
+"use server"
 
 const url = "http://localhost:3000/api/v1/q&a";
 export async function getAllData() {
   const res = await fetch(url);
   const data = await res.json();
   return data;
+}
+
+export async function postQues(ques) {
+  await fetch(url, { method: "POST", body: JSON.stringify(ques) });
 }
